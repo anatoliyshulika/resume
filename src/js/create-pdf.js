5c8f45e1c6b30291da02afdddb33f32df121402e
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2022-12-06 22:27:48 +0200

    Done

--- src/js/create-pdf.js
+++ src/js/create-pdf.js
@@ -3,9 +3,9 @@ const doc = html2pdf();
 
 const printedElement = document.querySelector(".container");
 const options = {
-  filename: "resume.pdf",
+  filename: "Anatolii_shulika_junior_FE-developer.pdf",
   image: { type: "jpeg", quality: 0.8 },
-  html2canvas: { scale: 4 },
+  html2canvas: { scale: 6 },
   jsPDF: {
     orientation: "p",
     unit: "px",
@@ -13,69 +13,12 @@ const options = {
     putOnlyUsedFonts: true,
     floatPrecision: 16,
     hotfixes: ["px_scaling"],
-    compress: true,
+    compress: false,
   },
 };
 
 const btnpdfRef = document.querySelector(".btn-pdf");
 btnpdfRef.addEventListener("click", () => {
   doc.set(options).from(printedElement).save();
-  // doc.from(printedElement).save("resume.pdf");
+  // window.print();
 });
-
-// =============================================
-// const printedElement = document.querySelector(".container");
-// const options = {
-//   margin: 0,
-//   filename: "resume.pdf",
-//   image: { type: "jpeg", quality: 0.9 },
-//   html2canvas: {
-//     scale: 3,
-//     with: 1200,
-//     heiht: 1223,
-//     // with: 210,
-//     // heiht: 297,
-//   },
-//   jsPDF: {
-//     orientation: "p",
-//     // unit: "px",
-//     unit: "mm",
-//     // format: [1200.0, 1467.0],
-//     format: "A4",
-//     // putOnlyUsedFonts: true,
-//     floatPrecision: 16,
-//     // hotfixes: ["px_scaling"],
-//     compress: true,
-//   },
-// };
-
-// const qwe = document.querySelector(".btn-pdf");
-// qwe.addEventListener("click", () => {
-//   html2pdf().set(options).from(printedElement).save();
-//   console.log("Hren hren");
-// });
-
-// =============================================
-
-// window.jsPDF = window.jspdf.jsPDF;
-
-// var doc = new jsPDF({
-//   putOnlyUsedFonts: true,
-// });
-
-// // Source HTMLElement or a string containing HTML.
-// var elementHTML = document.querySelector(".container");
-
-// const btnpdfRef = document.querySelector(".btn-pdf");
-// btnpdfRef.addEventListener("click", () => {
-//   doc.html(elementHTML, {
-//     callback: function (doc) {
-//       // Save the PDF
-//       doc.save("sample-document.pdf");
-//     },
-//     x: 2,
-//     y: 2,
-//     width: 210, //target width in the PDF document
-//     windowWidth: 1200, //window width in CSS pixels
-//   });
-// });
